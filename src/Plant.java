a1a362e6b0c67b4d66f58c8b26ce1d4dcefe6db5
Leigh-Riane Amsterdam <amsterdam.l@northeastern.edu>
2025-12-03 18:25:25 -0500

    adding in species file and attributes for plant

--- src/Plant.java
+++ src/Plant.java
@@ -2,6 +2,11 @@
  * This is the Plant class
  */
 public class Plant {
+  private String name;
+  private Species species;
+  private String lastWatered;
+  private String nextWaterDue;
+  private boolean isSold;
   /**
    * Constructor for plant class
    */
---
+++ src/Species.java
@@ -0,0 +1,12 @@
+/**
+ * This is the Species class.
+ */
+public class Species {
+  private String name;
+  private String waterIntervalDays;
+  private String sunlightNeeds;
+
+  public Species() {
+
+  }
+}
